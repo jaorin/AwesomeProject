f56980919cac649099c9f20806c3f02808288fa9
JAORIN <zaesar@outlook.com>
2021-05-09 11:49:31 +0700

    Initial commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import Welcome from './screens/Welcome';
+import Ex1 from './screens/Ex1';
+import Ex2 from './screens/Ex2';
+import Ex3 from './screens/Ex3';
+import Ex4 from './screens/Ex4';
+import Ex5 from './screens/Ex5';
+import Ex6 from './screens/Ex6';
+import Ex7 from './screens/Ex7';
+import Ex8 from './screens/Ex8';
+import Ex9 from './screens/Ex9';
+import Ex10 from './screens/Ex10';
+import Ex11 from './screens/Ex11';
+import Ex12 from './screens/Ex12';
+import Bmi from './screens/Bmi';
+
+export default function App() {
+  return (
+    <Bmi />    
+  );
+}
